8181effbc35bdfa60ca5a864d9a025d1073be7c8
Otto Gutierrez <ottogutierrez@Ottos-iMac.phub.net.cable.rogers.com>
2019-08-13 21:57:08 -0400

    test functionality

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const volleybal = require('volleyball')
 
 const app = express()
 app.use(volleybal)
-
+app.use(express.static('public'))
 const port = process.env.PORT || 3000
 
-app.get('/', (req,res)=> {
-    res.json("Hello from server")
-})
+// app.get('/', (req,res)=> {
+//     res.json("Hello from server")
+// })
 
 app.listen(port, ()=> console.log(`Server listening on port ${port}`))
 
